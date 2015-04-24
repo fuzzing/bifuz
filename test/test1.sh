51776f8b84143ae05e45c6a8e8fd747ea85afcb5
Razvan Ionescu <razvan.ionescu@intel.com>
2015-04-24 14:00:43 +0300

    removing the test folder - deprecated

--- test/test1.sh
+++
@@ -1,8 +0,0 @@
- am broadcast -n com.android.providers.media/com.android.providers.media.MediaScannerReceiver
- am broadcast -n com.android.providers.media/com.android.providers.media.MediaUpgradeReceiver
- am broadcast -n com.android.providers.media/com.android.providers.media.MtpReceiver
- am broadcast -n com.android.inputdevices/com.android.inputdevices.InputDeviceReceiver
- am broadcast -n com.google.android.wearable.app/com.google.android.clockwork.home.BootCompletedReceiver
- am broadcast -n android/com.android.server.updates.SELinuxPolicyInstallReceiver
- am broadcast -n android/com.android.server.updates.SmsShortCodesInstallReceiver
- am broadcast -n android/com.android.server.updates.TZInfoInstallReceiver
